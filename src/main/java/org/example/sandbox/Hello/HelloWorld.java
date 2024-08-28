3640ee2a54eb34c4c5ba5ce463597573e5410e48
wonderchex <jos2493657@maricopa.edu>
2024-08-27 20:08:08 -0700

    Adding Hello World

--- src/main/java/org/example/sandbox/Hello/HelloWorld.java
+++ src/main/java/org/example/sandbox/Hello/HelloWorld.java
@@ -1,8 +1,9 @@
-package org.example.sandbox;
+package org.example.sandbox.Hello;
 
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!!");
+        System.out.println("Hello World!");
     }
+
 }
---
+++ src/main/java/org/example/sandbox/Hello/World.java
@@ -0,0 +1,4 @@
+package org.example.sandbox.Hello;
+
+public class World {
+}
